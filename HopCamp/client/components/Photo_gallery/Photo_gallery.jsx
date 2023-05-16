3b69f7a9bccc494a0614d3c665d85646617ff63d
Kennedyz71 <kennedyz71@gmail.com>
2023-05-16 15:47:53 -0700

    adding Modal

--- HopCamp/client/components/Photo_gallery/Photo_gallery.jsx
+++ HopCamp/client/components/Photo_gallery/Photo_gallery.jsx
@@ -1,101 +1,144 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Photo_gallery.css'
 import 'bootstrap/dist/css/bootstrap.css';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+
+const pic1_URL = "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 256w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 384w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_550,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 550w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_740,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 740w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 950w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 1120w"
+const pic11_URL= "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg"
+const pic2_URL = "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 256w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 384w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_550,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 550w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_740,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 740w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 950w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 1120w"
+const pic22_URL= "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg"
+const pic3_URL = "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 256w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 384w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_550,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 550w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_740,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 740w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 950w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 1120w"
+const pic33_URL= "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg"
+const pic4_URL = "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj.jpg 256w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_60/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj.jpg 384w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_550,q_60/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj.jpg 550w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_740,q_60/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj.jpg 740w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj.jpg 950w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj.jpg 1120w"
+const pic44_URL= "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj.jpg"
+const pic5_URL = "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 256w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 384w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_550,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 550w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_740,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 740w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 950w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 1120w"
+const pic55_URL= "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg"
+
+
+
+
+
 
 function Photo_gallery() {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState('');
+
+  const openModal = (photoURL) => {
+    setSelectedPhoto(photoURL);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setSelectedPhoto('');
+  };
+
   return (
-    <div className="d-flex p-4 gap-3">      
-        {/* <img src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg" alt="big-photo1" /> */}
-        <img
-       alt=""
-         sizes="45vw"
-         srcSet="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 256w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 384w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_550,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 550w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_740,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 740w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 950w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 1120w"
-         src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg"
-         decoding="async"
-         data-nimg="fill"
-         className="photo-gallery-img"
-       />
-      <div>
-      <div className="row b-3">
-        <div className="col mb-2">
-        <img
-       alt=""
-         sizes="25vw,"
-         srcSet="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 256w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 384w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_550,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 550w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_740,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 740w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 950w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 1120w"
-         src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg"
-         decoding="async"
-         data-nimg="fill"
-         className="photo-gallery-img"
-       />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 gap-2 d-flex justify-content-center align-items-center">
+          <div className="photo-gallery-left">
+            <div className="large photo">
+              <button className="photo-gallery-button" onClick={() => openModal(pic1_URL)}>
+                <img
+                  alt=""
+                  sizes="40vw"
+                  srcSet={pic1_URL}
+                  src={pic11_URL}
+                  decoding="async"
+                  data-nimg="fill"
+                  className="photo-gallery-img"
+                />
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="col mt-2">
-        <img
-       alt=""
-         sizes="25vw"
-         srcSet="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj.jpg 256w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_60/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj.jpg 384w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_550,q_60/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj.jpg 550w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_740,q_60/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj.jpg 740w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj.jpg 950w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj.jpg 1120w"
-         src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj.jpg"
-         decoding="async"
-         data-nimg="fill"
-         className="photo-gallery-img"
-       />
-        </div>
-      </div>
-    </div>
-        <div className="row b-0, p-0">
-        <div className="col mb-2">
-        <img
-       alt=""
-         sizes="25vw"
-         srcSet="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 256w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 384w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_550,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 550w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_740,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 740w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 950w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 1120w"
-         src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg"
-         decoding="async"
-         data-nimg="fill"
-         className="photo-gallery-img"
-       />
-        </div>
-        <div className="col mt-2">
-        <img
-       alt=""
-         sizes="25vw"
-         srcSet="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 256w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 384w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_550,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 550w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_740,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 740w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 950w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 1120w"
-         src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg"
-         decoding="async"
-         data-nimg="fill"
-         className="photo-gallery-img"
-       />
-        </div>
+
+        <div className="col-lg-6">
+          <div className="photo-gallery-square">
+            <div className="row">
+              <div className="col-md-6 mb-2 d-flex justify-content-center justify-content-md-between">
+                <div className="photo-gallery-item">
+                  <button className="photo-gallery-button" onClick={() => openModal(pic2_URL)}>
+                    <img
+                      alt=""
+                      sizes="50vw"
+                      srcSet={pic2_URL}
+                      src={pic22_URL}
+                      decoding="async"
+                      data-nimg="fill"
+                      className="photo-gallery-img"
+                    />
+                  </button>
+                </div>
+              </div>
+              <div className="col-md-6 justify-content-center justify-content-md-between">
+                <div className="photo-gallery-item">
+                  <button className="photo-gallery-button" onClick={() => openModal(pic3_URL)}>
+                    <img
+                      alt=""
+                      sizes="50vw"
+                      srcSet={pic3_URL}
+                      src={pic33_URL}
+                      decoding="async"
+                      data-nimg="fill"
+                      className="photo-gallery-img"
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6 justify-content-center justify-content-md-between">
+                <div className="photo-gallery-item">
+                  <button className="photo-gallery-button" onClick={() => openModal(pic4_URL)}>
+                    <img
+                      alt=""
+                      sizes="50vw"
+                      srcSet={pic4_URL}
+                      src={pic44_URL}
+                      decoding="async"
+                      data-nimg="fill"
+                      className="photo-gallery-img"
+                    />
+                  </button>
+                </div>
+              </div>
+              <div className="col-sm-6 justify-content-center justify-content-md-between">
+                <div className="photo-gallery-item">
+                  <button className="photo-gallery-button" onClick={() => openModal(pic5_URL)}>
+                    <img
+                      alt=""
+                      sizes="50vw"
+                      srcSet={pic5_URL}
+                      src={pic55_URL}
+                      decoding="async"
+                      data-nimg="fill"
+                      className="photo-gallery-img"
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-  );
+      {/* Modal */}
+      <Modal show={showModal} onHide={closeModal}>
+        <Modal.Body>
+          <img src={selectedPhoto} alt="" className="modal-photo" />
+        </Modal.Body>
+        <Modal.Footer>
+        <Button variant="secondary" onClick={closeModal}>
+        Close
+        </Button>
+        </Modal.Footer>
+        </Modal>
+</div>
+);
 }
 
 export default Photo_gallery;
-
-// import React from 'react';
-// import './Photo_gallery.css'
-// import 'bootstrap/dist/css/bootstrap.css';
-
-// function Photo_gallery() {
-//   return (
-//     <div className="photo-gallery">
-//       <img
-//         alt=""
-//         sizes="45vw"
-//         srcSet="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 256w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 384w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_550,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 550w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_740,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 740w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 950w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 1120w"
-//         src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg"
-//         decoding="async"
-//         data-nimg="fill"
-//         className="photo-gallery-img"
-//       />
-//       <div className="photo-gallery-grid">
-//         <img src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_120,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg" alt="photo1" className="photo-gallery-grid-item" />
-//         <img src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_120,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg" alt="photo2" className="photo-gallery-grid-item" />
-//         <img src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_120,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg" alt="photo3" className="photo-gallery-grid-item" />
-//         <img src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_120,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg" alt="photo4" className="photo-gallery-grid-item" />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Photo_gallery;
