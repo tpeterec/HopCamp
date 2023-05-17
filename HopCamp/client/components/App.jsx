d43033fe4e9bd66f91f741efd760530d0b976dd0
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-05-17 12:36:36 -0700

    added my components to App.jsx

--- HopCamp/client/components/App.jsx
+++ HopCamp/client/components/App.jsx
@@ -3,6 +3,9 @@ import "./App.css"
 import 'bootstrap/dist/css/bootstrap.css';
 import Current_campsite from './Current_campsite/Current_campsite.jsx'
 import Available_sites from './Available_sites/Available_sites.jsx'
+import Header from "./Header/Header.jsx";
+import Ratings from "./Ratings/Ratings";
+import Camping_spots from "./Camping_spots/Camping_spots";
 
 function App() {
   const handleClick=(event)=>{
@@ -15,23 +18,28 @@ function App() {
 
   return (
     <div id='contentWrapper' onClick={handleClick}>
-    <div className='header'>
-      <h1>Header</h1>
-    </div>
- 
-    <Current_campsite />
-    <h1>Photo Gallery</h1>
-    <h1>Current Campsite</h1>
-    <h1>Camp Info</h1>
-    <Available_sites />
-    <h1>Tent Sites (plus map)</h1>
-    <h1>Camping Location</h1>
-    <h1>Location</h1>
-    <h1>Hosted by</h1>
-    <h1>Camping Spots</h1>
-    <h1>Camp Safety</h1>
-    <h1>Safety & Partners</h1>
-    <h1>Footer</h1>
+      <div>
+        <Header />
+      </div>
+      <Current_campsite />
+      <h1>Photo Gallery (Dennis)</h1>
+      <h1>Current Campsite (Chris)</h1>
+      <h1>Camp Info (Chris)</h1>
+      <Available_sites />
+      <h1>Tent Sites (Chris)</h1>
+      <h1>Camping Location (Dan)</h1>
+      <h1>Location (Dan)</h1>
+      <h1>Hosted by (Dennis)</h1>
+      <div>
+        <Ratings />
+      </div>
+      <h1>Things to do (Dan)</h1>
+      <div>
+        <Camping_spots />
+      </div>
+      <h1>Camp Safety (Dennis)</h1>
+      <h1>Safety & Partners (Dan)</h1>
+      <h1>Footer (Chris)</h1>
     </div>
   )
 }
