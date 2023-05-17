871b36f19f888064f47f5a1364a1b3f85cf7c803
Kennedyz71 <kennedyz71@gmail.com>
2023-05-17 13:17:10 -0700

    added modal for photogallery, needs to be full screen

--- HopCamp/client/components/Photo_gallery/Photo_gallery.jsx
+++ HopCamp/client/components/Photo_gallery/Photo_gallery.jsx
@@ -6,7 +6,7 @@ import Button from 'react-bootstrap/Button';
 
 
 const pic1_URL = "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 256w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 384w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_550,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 550w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_740,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 740w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 950w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg 1120w"
-const pic11_URL= "https://www.hipcamp.com/en-US/land/california-salmon-creek-ranch-4kk9hl90/photos?"
+const pic11_URL= "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1476482092/campground-photos/abvqx8gl26smow3uxgk8.jpg"
 const pic2_URL = "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 256w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 384w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_550,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 550w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_740,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 740w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 950w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg 1120w"
 const pic22_URL= "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1467751641/campground-photos/shu8s1oyjqjrgsmcc6gm.jpg"
 const pic3_URL = "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 256w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 384w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_550,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 550w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_740,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 740w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 950w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1447142502/campground-photos/waaje5sol4bzjaellxgp.jpg 1120w"
@@ -15,7 +15,11 @@ const pic4_URL = "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v
 const pic44_URL= "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj.jpg"
 const pic5_URL = "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 256w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 384w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_550,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 550w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_740,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 740w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 950w, https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg 1120w"
 const pic55_URL= "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/v1445485065/campground-photos/p4mfmzsuapsb8hqs7wg2.jpg"
-
+const pic66_URL= "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_80/v1476482090/campground-photos/pyjxig2abgalkhh6kpdm.jpg"
+const pic77_URL= "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_80/v1472247288/campground-photos/xtuutkiep3ekc3t1n9wn.jpg"
+const pic88_URL= "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_80/v1445485060/campground-photos/p1gizt29wlal2thvnlt2.jpg"
+const pic99_URL= "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_80/v1476483277/campground-photos/fuhi5sbqwy2otsylq9qd.jpg"
+const pic1010_URL= "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_80/v1470530386/campground-photos/osu51frsvlodi52yjp41.jpg"
 
 
 
@@ -24,6 +28,7 @@ const pic55_URL= "https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_1120,q_60/
 function Photo_gallery() {
   const [showModal, setShowModal] = useState(false);
   const [selectedPhoto, setSelectedPhoto] = useState('');
+  const photoGallery = [pic11_URL, pic22_URL, pic33_URL, pic44_URL, pic55_URL, pic66_URL, pic77_URL, pic88_URL, pic99_URL, pic1010_URL]
 
   const openModal = (photoURL) => {
     setSelectedPhoto(photoURL);
@@ -36,22 +41,22 @@ function Photo_gallery() {
   };
 
   return (
-    <div className="container" style={{ maxWidth: '50vw', maxHeight: '100%' }}>
+    <div className="container" style={{ maxWidth: '70vw'}}>
       <div className="display" >
       <div className="row">
         <div className="col-md-6 d-flex justify-content-center align-items-center">
           <div overflow="hidden">
-          <div className="photo-gallery-left" style={{overflow:'hidden', marginLeft: '-8px'}}>
+          <div className="photo-gallery-left" style={{overflow:'hidden', marginLeft: '-28px'}}>
             <div className="large photo">
-              <button className="photo-gallery-button" onClick={() => openModal(pic11_URL)}>
+              <button className="photo-gallery-button" onClick={() => openModal(photoGallery)}>
                 <img
                   alt=""
-                  sizes="40vw"
+                  sizes="45vw"
                   srcSet={pic1_URL}
                   decoding="async"
                   data-nimg="fill"
                   className="photo-gallery-img"
-                  style={{ maxWidth: '100%', maxHeight: '100%' }}
+                  // style={{ maxWidth: '100%', maxHeight: '100%' }}
                 />
               </button>
             </div>
@@ -59,34 +64,34 @@ function Photo_gallery() {
           </div>
         </div>
 
-        <div className="col-md-6">
-          <div className="photo-gallery-square" style={{ overflow: 'hidden', marginLeft: '-18px' }}>
+        <div className="col-sm-6">
+          <div className="photo-gallery-square" style={{ maxWidth: '50vw', overflow: 'hidden', marginLeft: '-18px' }}>
              <div className="row">
-              <div className="col-md-6 mb-2 d-flex justify-content-center justify-content-md-between">
+              <div className="col-sm-6 mb-2 d-flex justify-content-center justify-content-md-between">
                 <div className="photo-gallery-item" style={{ display: 'flex', justifyContent: 'center', marginRight: '-25px' }}>
-                  <button className="photo-gallery-button" onClick={() => openModal(pic2_URL)}>
+                  <button className="photo-gallery-button" onClick={() => openModal(photoGallery)}>
                     <img alt="" 
-                    sizes="50vw"
+                    sizes="45vw"
                     srcSet={pic2_URL}
                     decoding="async"
                     data-nimg="fill"
                     className="photo-gallery-img"
-                    style={{ maxWidth: '100%', maxHeight: '100%' }}
+                    // style={{ maxWidth: '100%', maxHeight: '100%' }}
                     />
                   </button>
                 </div>
               </div>
               <div className="col-md-6 justify-content-center justify-content-md-between">
                <div className="photo-gallery-item" style={{ display: 'flex', justifyContent: 'center', marginRight: '-18px' }}>
-                  <button className="photo-gallery-button" onClick={() => openModal(pic3_URL)}>
+                  <button className="photo-gallery-button" onClick={() => openModal(photoGallery)}>
                    <img
                    alt=""
-                   sizes="50vw"
+                   sizes="45vw"
                     srcSet={pic3_URL}
                     decoding="async"
                     data-nimg="fill"
                     className="photo-gallery-img"
-                    style={{ maxWidth: '100%', maxHeight: '100%' }}
+                    // style={{ maxWidth: '100%', maxHeight: '100%' }}
                     />
                  </button>
                 </div>
@@ -95,10 +100,10 @@ function Photo_gallery() {
               <div className="row">
                 <div className="col-md-6 justify-content-center justify-content-md-between">
                   <div className="photo-gallery-item" style={{ display: 'flex', justifyContent: 'center', marginRight:'19px' }}>
-                    <button className="photo-gallery-button" onClick={() => openModal(pic4_URL)}>
+                    <button className="photo-gallery-button" onClick={() => openModal(photoGallery)}>
                       <img
                       alt=""
-                      sizes="50vw"
+                      sizes="45vw"
                       srcSet={pic4_URL}
                       decoding="async"
                       data-nimg="fill"
@@ -109,16 +114,16 @@ function Photo_gallery() {
                 </div>
                 <div className="col-md-6 justify-content-center justify-content-md-between">
                   <div className="photo-gallery-item" style={{ display: 'flex', justifyContent: 'center', marginRight: '28px' }}>
-                    <button className="photo-gallery-button" onClick={() => openModal(pic55_URL)}>
+                    <button className="photo-gallery-button" onClick={() => openModal(photoGallery)}>
                       <img
                       alt=""
-                      sizes="50vw"
+                      sizes="45vw"
                       srcSet={pic5_URL}
                       src={pic55_URL}
                       decoding="async"
                       data-nimg="fill"
                       className="photo-gallery-img"
-                      style={{ maxWidth: '100%', maxHeight: '100%' }}
+                      // style={{ maxWidth: '50vw', maxHeight: '100%' }}
                       />
                     </button>
                   </div>
@@ -128,20 +133,34 @@ function Photo_gallery() {
           </div>
 
        {/*modal*/}
-       <Modal show={showModal} onHide={closeModal}>
-         <Modal.Body>
-           <img src={selectedPhoto} alt="" className="modal-photo" />
-           <p>{selectedPhoto}</p> {/* Display the selected photo URL */}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={closeModal}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
+       <Modal show={showModal} onHide={closeModal} className="modal-dialog modal-fullscreen">
+         <Modal.Body className="modal-body-photo-gallery">
+           <div className="photo-gallery-grid">
+             {photoGallery.map((photoURL, index) => (
+               <img
+                 key={index}
+                 src={photoURL}
+                 alt=""
+                 className="modal-photo"
+               />
+             ))}
+           </div>
+         </Modal.Body>
+         <Modal.Footer>
+           <Button variant="secondary" onClick={closeModal} className="modal-close-button">
+           &lt; {/* The arrow symbol */}
+           </Button>
+         </Modal.Footer>
+        </Modal>
+
+      
+
+
      </div> 
   </div>
   </div>
+
+
 );
 }
 
