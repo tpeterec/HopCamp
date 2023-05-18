2aeff815fdde3a1e5d72f0d47b4c5044b1786fa0
DessertWarrior <gly310@gmail.com>
2023-05-18 12:44:47 -0700

    added: migration.sql and seed.sql. Changes from hard coded information to database fetch

--- HopCamp/client/components/Tent_site/Tent_site.jsx
+++ HopCamp/client/components/Tent_site/Tent_site.jsx
@@ -1,47 +1,52 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Tent_site.css'
 import Individual_Tent_site from './Individual_Tent_site';
 import Individual_Lodging_site from './Individual_Lodging_site';
 
 // import site1_campground from '/HopCamp/HopCamp/client/assets/site1 campground.webp'
 const Tent_site = () => {
+    const [tentsiteData,setTentSiteData]=useState([]);
+    // const tentSiteData = {
+    //     id: 1,
+    //     name: 'Site 1- Redwood Camp',
+    //     availability: '5 available',
+    //     demand: 'High demand',
+    //     instantBook: 'Instant book',
+    //     rating: '99%',
+    //     reviews: '(361)',
+    //     type: 'RV/tent site',
+    //     capacity: '· Sleeps 8',
+    //     description: 'NOTE REGARDING ACCESS: 4WD or AWD WITH GOOD GROUND CLEARANCE IS REQUIRED due to steep, rutted farm road with loose gravel in summer and mud in winter.  Tesla model Y and similar vehicles that may be called "SUVs" but are very low to the ground are not allowed.  If you need to be towed back up in your non-4WD/AWD vehicle, we will charge you $100 for our time and expertise.  AAA and other roadside assistant services will not be able to help you. Regular RVs not allowed, but 4WD camper vans are OK. Small pop-up and teardrop trailers with 4WD tow vehicles by negotiation. Check with us first. ',
+    //     dealPrice: '96',
+    //     regularPrice: '120'
+    // }
     
-    const tentSiteData = {
-        id: 1,
-        name: 'Site 1- Redwood Camp',
-        availability: '5 available',
-        demand: 'High demand',
-        instantBook: 'Instant book',
-        rating: '99%',
-        reviews: '(361)',
-        type: 'RV/tent site',
-        capacity: '· Sleeps 8',
-        description: 'NOTE REGARDING ACCESS: 4WD or AWD WITH GOOD GROUND CLEARANCE IS REQUIRED due to steep, rutted farm road with loose gravel in summer and mud in winter.  Tesla model Y and similar vehicles that may be called "SUVs" but are very low to the ground are not allowed.  If you need to be towed back up in your non-4WD/AWD vehicle, we will charge you $100 for our time and expertise.  AAA and other roadside assistant services will not be able to help you. Regular RVs not allowed, but 4WD camper vans are OK. Small pop-up and teardrop trailers with 4WD tow vehicles by negotiation. Check with us first. ',
-        dealPrice: '96',
-        regularPrice: '120'
-    }
-    
-    const lodgingData = {
-        id: 1,
-        name: "Eagle's Nest Treehouse Farmstay",
-        availability: "0 available",
-        demand: "High demand",
-        instantBook: "Instant book",
-        rating: "99%",
-        reviews: "(339)",
-        type: "Treehouse ·",
-        capacity: "Sleeps 2",
-        description: "The Eagle's Nest Treehouse Farm Stay offers ..."
-    }
-    const arr = Array(10).fill(tentSiteData);
+    // const lodgingData = {
+    //     id: 1,
+    //     name: "Eagle's Nest Treehouse Farmstay",
+    //     availability: "0 available",
+    //     demand: "High demand",
+    //     instantBook: "Instant book",
+    //     rating: "99%",
+    //     reviews: "(339)",
+    //     type: "Treehouse ·",
+    //     capacity: "Sleeps 2",
+    //     description: "The Eagle's Nest Treehouse Farm Stay offers ..."
+    // }
+    useEffect(()=>{
+        fetch('http://localhost:5000/api/campsites').then(responses=>responses.json())
+        .then(result=>{
+            setTentSiteData(result);
+        })
+    },[])
   return (
     <div className='tent-sites'>
         <div className='tent-sites-title'> 
             <div id="tentsite-title">Tent sites</div>
             <div id="tentsite-availability"> 5 available</div>
         </div>
-        {arr.map((element, index) => (
+        {tentsiteData.map((element, index) => (
             <Individual_Tent_site key={index} data={element}/>
 
         ))}
