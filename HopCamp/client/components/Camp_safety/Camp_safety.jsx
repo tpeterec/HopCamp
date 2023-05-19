2493d907c5f8cae6195b0bcec667a34a74832f8a
Kennedyz71 <kennedyz71@gmail.com>
2023-05-19 11:12:24 -0700

    adding campSafety

--- HopCamp/client/components/Camp_safety/Camp_safety.jsx
+++ HopCamp/client/components/Camp_safety/Camp_safety.jsx
@@ -1,57 +1,67 @@
 import React, { useState, useEffect } from "react";
-import './Camp_safety.css';
+import "./Camp_safety.css";
 
-function Camp_safety(){
-    return (
-        <div className="safety-component-container">
-            <h1 className="safety-heading">Safety at Hopcamp</h1>
-            <div className="safety-card-container">
-                <div className="safety-card">
-                    <div className="safety-card-content">
-                        <img 
-                        src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_auto/internal/inclusion_policy_text_narrow.png"
-                        className="safety-card-image"
-                        ></img>
-                        <img src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_384,q_auto/hipcamp_hand_hexqba.png"
-                        className="hipcamp-hand"/>
-                    </div>
-                    {/* <div className="card_button"> */}
-                        <a
-                        className="safety-card-button"
-                        href="https://support.hipcamp.com/hc/en-us/articles/360049636632/"
-                        >Learn More</a>
-                    {/* </div> */}
-                </div>
-                <div className="safety-card">
-                    <div className="safety-card-content">
-                        <img
-                        src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_auto/internal/hosting_standards_text.png"
-                        className="safety-card-image"
-                        ></img>
-                    </div>
-                    <a
-                    className="safety-card-button"
-                    href="https://www.hipcamp.com/en-US/hoststandards"
-                    >Learn More</a>
-                </div>
-                <div className="safety-card">
-                    <div className="safety-card-content">
-                        <img
-                        src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_auto/internal/hipcamper_standards_text.png"
-                        className="safety-card-image"
-                        ></img>
-                    </div>
-                    <a
-                    className="safety-card-button"
-                    href="https://www.hipcamp.com/en-US/hipcamperstandards"
-                    >Learn More</a>
-                </div>
+function Camp_safety() {
+  return (
+    <div className="safety-component-container">
+      <div className="safety-heading">Safety at Hopcamp</div>
+      <div className="safety-card-container">
+        <div className="safety-card">
+          <div className="safety-card-content">
+            <img
+              src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_auto/internal/inclusion_policy_text_narrow.png"
+              className="safety-card-image"
+              altName="top1"
+            ></img>
+            <div className="logo">
+              <img
+                src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_256,q_auto/hipcamp_hand_hexqba.png"
+                className="hipcamp-hand"
+              />
             </div>
+          </div>
+          {/* <div className="card_button"> */}
+          <a
+            className="safety-card-button"
+            href="https://support.hipcamp.com/hc/en-us/articles/360049636632/"
+          >
+            Learn More
+          </a>
+          {/* </div> */}
         </div>
-    );
+        <div className="safety-card">
+          <div className="safety-card-content">
+            <img
+              src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_auto/internal/hosting_standards_text.png"
+              className="safety-card-image"
+              altName="bot2"
+            ></img>
+          </div>
+          <a
+            className="safety-card-button"
+            href="https://www.hipcamp.com/en-US/hoststandards"
+          >
+            Learn More
+          </a>
+        </div>
+        <div className="safety-card">
+          <div className="safety-card-content">
+            <img
+              src="https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_auto/internal/hipcamper_standards_text.png"
+              className="safety-card-image"
+              altName="bot3"
+            ></img>
+          </div>
+          <a
+            className="safety-card-button"
+            href="https://www.hipcamp.com/journal/company-news/fire-safety-introducing-national-weather-service-integration"
+          >
+            Learn More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-
-
-export default Camp_safety
+export default Camp_safety;
