296fddbddd63843e9a8dd609be7d1a62f135c62e
DanielPurdy509 <danielpurdy509@gmail.com>
2023-05-22 17:20:35 -0700

    Things todo nearby component finished

--- HopCamp/server/db/Things_todo_nearby/things_nearby_seed.sql
+++ HopCamp/server/db/Things_todo_nearby/things_nearby_seed.sql
@@ -1,7 +1,9 @@
 INSERT INTO things_nearby (img, title, distance) VALUES
-    ('./assets/Bodega_Head.jpg','Bodega Head','4 miles away'),
-    ('./assets/Armstrong_Redwoods_State_Natural_Reserve.jpg','Armstrong Redwoods State Natural Reserve','12 miles away'),
-    ('./Marconi_Conference_Center_State_Historic_Park.jpg','Marconi Conference Center State Historic Park','16 miles away'),
-    ('./Salt_Point_State_Park.jpg','Salt Point State Park','23 miles away'),
-    ('./Jack_London_State_Historic_Park.jpg','Jack London State Historic Park','24 miles away'),
-    ('./Kruse_Rhododendron_State_Natural_Reserve.jpg','Kruse Rhododendron State Natural Reserve','24 miles away');
+    ('https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1655602609/park_images/poi/us/s8wg6jkl2uo1h241bcsh.jpg','Bodega Head','4 miles away'),
+    ('https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1646262040/park_images/poi/us/senhj6traburp1ftwfmo.jpg','Armstrong Redwoods State Natural Reserve','12 miles away'),
+    ('https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1639636733/park_images/poi/us/kkddfa35ggwbckdpv1za.jpg','Marconi Conference Center State Historic Park','16 miles away'),
+    ('https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1424593552/campground-photos/pt1f22v5wfy6gprggina.jpg','Salt Point State Park','23 miles away'),
+    ('https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1639636713/park_images/poi/us/ykoomrxz9edlchi0akvf.jpg','Jack London State Historic Park','24 miles away'),
+    ('https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1639636719/park_images/poi/us/yz9kfrgtijkticauivfr.jpg','Kruse Rhododendron State Natural Reserve','24 miles away'),
+    ('https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1639636739/park_images/poi/us/h7trzfxbnvrae8rhtohi.jpg','Olompali State Historic Park','27 miles away'),
+    ('https://hipcamp-res.cloudinary.com/f_auto,c_limit,w_950,q_60/v1656091995/park_images/poi/us/hptjpoye6iqc9g8fhfto.jpg','Calistoga Hot Springs','27 miles away');
