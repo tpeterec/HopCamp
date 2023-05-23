407cbc76e1b19b4ab0477f276b1f8390b60e71a3
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-05-22 18:08:52 -0700

    changed port to 5001

--- HopCamp/client/components/Tent_site/Tent_site.jsx
+++ HopCamp/client/components/Tent_site/Tent_site.jsx
@@ -54,7 +54,7 @@ const Tent_site = () => {
     };
 
     useEffect(()=>{
-        fetch('http://localhost:5000/api/campsites').then(responses=>responses.json())
+        fetch('http://localhost:5001/api/campsites').then(responses=>responses.json())
         .then(result=>{
             let rvArr= [];
             let lodgingArr = [];
