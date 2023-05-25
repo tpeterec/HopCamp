aa7b9f10afed5e4255aabcc541a5ce826074b6e9
tpeterec <alex1996p3@gmail.com>
2023-05-25 15:19:01 -0800

    adjusted .env file for local machine. Did first sample test

---
+++ HopCamp/script.js
@@ -0,0 +1,22 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export const options = {
+  stages: [
+    { duration: "30s", target: 20 },
+    { duration: "1m30s", target: 10 },
+    { duration: "20s", target: 0 },
+  ],
+};
+
+export default function () {
+  http.get("http://localhost:5001/api/campsites");
+  http.get("http://localhost:5001/api/ratings");
+  http.get("http://localhost:5001/api/camping-spots");
+  http.get("http://localhost:5001/api/campers-also");
+  http.get("http://localhost:5001/api/photogallery");
+  http.get("http://localhost:5001/api/things-nearby");
+//   const res = http.get("http://localhost:5001/api/campsites");
+//   check(res, { "status was 200": (r) => r.status == 200 });
+//   sleep(1);
+}
